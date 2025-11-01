ad5155dc1d8c4a42b33373ae1c29fc2c82345c88
theleonkim <kleon80701@ufide.ac.cr>
2025-10-31 19:35:44 -0600

    chore: agrega carpeta PF con estructura base

---
+++ PF/server/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Servidor Quixo funcionando ✅");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
